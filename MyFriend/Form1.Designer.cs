0c8cf18d288caa7c91c51ee8b96e42ab68908c09
Doooo0 <97488185+dooo0@users.noreply.github.com>
2023-12-24 15:54:36 +0900

    Add files via upload

---
+++ MyFriend/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace MyFriend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.불러오기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.끝내기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.저장SeriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.불러오기SeriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(74, 103);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "남성";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(74, 135);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 16);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "여성";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "이름 : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "나이 : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "성별 : ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(74, 63);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(125, 21);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 71);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "친구추가";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(126, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 71);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "친구삭제";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(270, 292);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "친구입력";
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.LabelEdit = true;
+            this.listView1.Location = new System.Drawing.Point(301, 46);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(368, 287);
+            this.listView1.TabIndex = 11;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.AfterLabelEdit += new System.Windows.Forms.LabelEditEventHandler(this.listView1_AfterLabelEdit);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "이름";
+            this.columnHeader1.Width = 189;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "나이";
+            this.columnHeader2.Width = 88;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "성별";
+            this.columnHeader3.Width = 87;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(681, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.저장ToolStripMenuItem,
+            this.불러오기ToolStripMenuItem,
+            this.저장SeriToolStripMenuItem,
+            this.불러오기SeriToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.끝내기ToolStripMenuItem});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // 저장ToolStripMenuItem
+            // 
+            this.저장ToolStripMenuItem.Name = "저장ToolStripMenuItem";
+            this.저장ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.저장ToolStripMenuItem.Text = "저장";
+            this.저장ToolStripMenuItem.Click += new System.EventHandler(this.저장ToolStripMenuItem_Click);
+            // 
+            // 불러오기ToolStripMenuItem
+            // 
+            this.불러오기ToolStripMenuItem.Name = "불러오기ToolStripMenuItem";
+            this.불러오기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.불러오기ToolStripMenuItem.Text = "불러오기";
+            this.불러오기ToolStripMenuItem.Click += new System.EventHandler(this.불러오기ToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // 끝내기ToolStripMenuItem
+            // 
+            this.끝내기ToolStripMenuItem.Name = "끝내기ToolStripMenuItem";
+            this.끝내기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.끝내기ToolStripMenuItem.Text = "끝내기";
+            this.끝내기ToolStripMenuItem.Click += new System.EventHandler(this.끝내기ToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // 저장SeriToolStripMenuItem
+            // 
+            this.저장SeriToolStripMenuItem.Name = "저장SeriToolStripMenuItem";
+            this.저장SeriToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.저장SeriToolStripMenuItem.Text = "저장(Seri)";
+            this.저장SeriToolStripMenuItem.Click += new System.EventHandler(this.저장SeriToolStripMenuItem_Click);
+            // 
+            // 불러오기SeriToolStripMenuItem
+            // 
+            this.불러오기SeriToolStripMenuItem.Name = "불러오기SeriToolStripMenuItem";
+            this.불러오기SeriToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.불러오기SeriToolStripMenuItem.Text = "불러오기(Seri)";
+            this.불러오기SeriToolStripMenuItem.Click += new System.EventHandler(this.불러오기SeriToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 345);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "친구목록";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 저장ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 불러오기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 끝내기ToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem 저장SeriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 불러오기SeriToolStripMenuItem;
+    }
+}
+
---
+++ MyFriend/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFriend
+{
+    public partial class Form1 : Form
+    {
+        List<Friend> MyFriendList = new List<Friend>();
+        Friend MyFriend;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //친구 추가 버튼
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] strArrFriend = new string[] { textBox1.Text, numericUpDown1.Value.ToString(), radioButton1.Checked ? "남성" : "여성" };
+            MyFriend = new Friend(textBox1.Text, Convert.ToInt32(numericUpDown1.Value), radioButton1.Checked ? true : false);
+            MyFriendList.Add(MyFriend);
+            ListViewItem item = new ListViewItem(strArrFriend);
+            listView1.Items.Add(item);
+            textBox1.Text = "";
+            numericUpDown1.Value = 0;
+        }
+
+        //친구 삭제 버튼
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = listView1.Items.Count - 1; i >= 0; i--)
+            {
+                if (listView1.Items[i].Checked)
+                {
+                    listView1.Items[i].Remove();
+                    MyFriendList.RemoveAt(i);
+                }
+            }
+        }
+
+        //친구 수정 (LabelEdit)
+        private void listView1_AfterLabelEdit(object sender, LabelEditEventArgs e)
+        {
+            //e.Item.ToString(); //수정한Index
+            //e.Label; 수정내용
+            listView1.Items[e.Item].Text = e.Label;
+        }
+
+        //메뉴 - 끝내기
+        private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //메뉴 - 파일 저장하기
+        private void 저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var ret = saveFileDialog1.ShowDialog();
+            if (ret != DialogResult.OK) return;
+            FileStream fs = new FileStream(saveFileDialog1.FileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+
+            bw.Write(MyFriendList.Count());
+            foreach (var fr in MyFriendList)
+                   fr.WriteInfo(bw);
+            fs.Close();
+        }
+
+        //파일 - 불러오기
+        private void 불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var ret = openFileDialog1.ShowDialog();
+            if (ret != DialogResult.OK) return;
+
+            //기존 데이터 삭제
+            listView1.Items.Clear();
+            MyFriendList.Clear();
+
+            try
+            {
+                FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+
+                int nCount = br.ReadInt32();
+
+                for (int i = 0; i < nCount; i++)
+                {
+                    MyFriendList.Add(Friend.ReadInfo(br));
+                    string[] strArrFriend = new string[] { MyFriendList[i].m_strName, MyFriendList[i].m_nAge.ToString(), MyFriendList[i].m_bGender ? "남성" : "여성" };
+                    ListViewItem item = new ListViewItem(strArrFriend);
+                    listView1.Items.Add(item);
+                }
+                fs.Close();
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message); }
+        }
+
+        //친구 클래스
+        [Serializable]
+        class Friend
+        {
+            public string m_strName;
+            public int m_nAge;
+            public bool m_bGender;
+
+            //생성자
+            public Friend(string _strName, int _nAge, bool _Gender)
+            {
+                m_strName = _strName;
+                m_nAge = _nAge;
+                m_bGender = _Gender;
+            }
+
+            //Freind 저장
+            public void WriteInfo(BinaryWriter bw)
+            {
+                bw.Write(m_strName);
+                bw.Write(m_nAge);
+                bw.Write(m_bGender);
+            }
+
+            //Friend 불러오기
+            public static Friend ReadInfo(BinaryReader br)
+            {
+                string m_strName = br.ReadString();
+                int m_nAge = br.ReadInt32();
+                bool m_bGender = br.ReadBoolean();
+                return new Friend(m_strName, m_nAge, m_bGender);
+            }
+        }
+
+        private void 저장SeriToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var ret = saveFileDialog1.ShowDialog();
+            if (ret != DialogResult.OK) return;
+            FileStream fs = new FileStream(saveFileDialog1.FileName, FileMode.Create, FileAccess.Write);
+            BinaryFormatter bf = new BinaryFormatter();
+
+            bf.Serialize(fs, MyFriendList.Count());
+            bf.Serialize(fs, MyFriendList);
+            fs.Close();
+        }
+
+        private void 불러오기SeriToolStripMenuItem_Click(object sender, EventArgs e)
+        {    
+            var ret = openFileDialog1.ShowDialog();
+            if (ret != DialogResult.OK) return;
+
+            //기존 데이터 삭제
+            listView1.Items.Clear();
+            MyFriendList.Clear();
+            try
+            {
+                FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read);
+                BinaryFormatter bf = new BinaryFormatter();
+
+                int nCount = (int)bf.Deserialize(fs);
+                MyFriendList = (List<Friend>)bf.Deserialize(fs);
+                for (int i = 0; i < nCount; i++)
+                {
+                    string[] strArrFriend = new string[] { MyFriendList[i].m_strName, MyFriendList[i].m_nAge.ToString(), MyFriendList[i].m_bGender ? "남성" : "여성" };
+                    ListViewItem item = new ListViewItem(strArrFriend);
+                    listView1.Items.Add(item);
+                }
+                fs.Close();
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex.Message); }
+        }
+    }
+}
